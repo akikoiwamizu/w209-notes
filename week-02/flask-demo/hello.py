75a3fa4992f10451f4b0a9b3daa5a9eb9a691cc8
akikoiwamizu <akikoiwamizu@gmail.com>
2022-05-11 09:45:31 -0400

    flask notes updated

--- week-02/flask-demo/hello.py
+++ week-02/flask-demo/hello.py
@@ -4,3 +4,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "Hello World!"
+
+@app.route('/api/')
+@app.route('/api/<filename>/')
+def api(filename=None):
+    if filename:
+        # import pandas as pd
+        # df = pd.read_csv(filename)
+        # return df.to_dict()
+        d = {'a': 1, 'b': 2, 'c': filename}
+        return d
+    else:
+        # make a database query
+        # import SQLAlchemy as sa
+        d = {'a': 2, 'b': 3}
+        return(d, 200, {'header-a': 'b'})
+
+if __name__ == '__main__':
+    app.run(dev=False)
